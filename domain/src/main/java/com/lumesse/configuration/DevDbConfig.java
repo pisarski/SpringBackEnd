154ae23e9022448fed7ae65ec2facc0b3005ea5c
Sebastian Pisarski <sebastian.pisarski@lumesse.com>
2015-05-30 19:17:24 +0200

    add dev env data init script

--- domain/src/main/java/com/lumesse/configuration/DevDbConfig.java
+++ domain/src/main/java/com/lumesse/configuration/DevDbConfig.java
@@ -2,11 +2,15 @@ package com.lumesse.configuration;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
@@ -15,7 +19,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 public class DevDbConfig {
 
 	@Bean
-	public DataSource devDataSource() {
+	public DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL)
 				.build();
 	}
@@ -27,6 +31,22 @@ public class DevDbConfig {
 		adapter.setGenerateDdl(true);
 		adapter.setDatabasePlatform("org.hibernate.dialect.HSQLDialect");
 		return adapter;
+	}
+
+	@Profile("dev")
+	@Bean
+	public ResourceDatabasePopulator databasePopulator() {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.setSqlScriptEncoding("UTF-8");
+		populator.addScript(new ClassPathResource("dev_setup_data.sql"));
+		return populator;
+	}
+
+	@Profile("dev")
+	@Bean
+	public InitializingBean populatorExecutor() {
+		return () -> DatabasePopulatorUtils.execute(databasePopulator(),
+				dataSource());
 	}
 
 }
---
+++ domain/src/main/resources/dev_setup_data.sql
@@ -0,0 +1,1 @@
+insert into spittle(message, time) values('test', now());
