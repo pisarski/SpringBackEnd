f3fa0ec549423e03ed40505757cca656b8938aae
Sebastian Pisarski <sebastian.pisarski@lumesse.com>
2015-05-30 15:27:38 +0200

    Add apache tails, resource sharing, property files and internationalization.

--- core/src/main/java/com/lumesse/configuration/RootConfiguration.java
+++ core/src/main/java/com/lumesse/configuration/RootConfiguration.java
@@ -2,9 +2,14 @@ package com.lumesse.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @ComponentScan(basePackages = "com.lumesse.configuration")
+@PropertySources({
+		@PropertySource(value = "classpath:/properties/default.properties"),
+		@PropertySource(value = "file:${APP_CONFIG_DIR}/settings.properties", ignoreResourceNotFound = true) })
 public class RootConfiguration {
 
 }
--- webapp/src/main/webapp/WEB-INF/views/home.jsp
+++ webapp/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,15 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ page session="false"%>
-<html>
-<head>
-<title>Spittr</title>
-<link rel="stylesheet" type="text/css"
-	href="<c:url value="/resources/style.css" />">
-</head>
-<body>
-	<h1>Welcome to Spittr - Signum test</h1>
-	
-	<a href="<c:url value="/spittle/list" />">Spittles</a> |
-	<a href="<c:url value="/spitter/register" />">Register</a>
-</body>
-</html>
+<h1>Welcome to Spittr - Signum test</h1>
+
+<a href="<c:url value="/spittle/list" />">Spittles</a> |
+<a href="<c:url value="/spitter/register" />">Register</a>
