663e302aa198fa25a40e3f2e2f6c73cdca43812c
Sebastian Pisarski <sebastian.pisarski@lumesse.com>
2015-06-21 19:59:52 +0200

    fix issue with postresql

--- domain/src/main/java/com/lumesse/entity/User.java
+++ domain/src/main/java/com/lumesse/entity/User.java
@@ -13,12 +13,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.lumesse.entity.enums.UserRole;
 
 @Entity
+@Table(name = "SpittrUser")
 public class User extends BaseEntity {
 
 	private static final long serialVersionUID = 398585519823800766L;
