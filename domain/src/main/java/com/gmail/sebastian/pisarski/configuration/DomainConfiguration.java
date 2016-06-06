7a7273f58d5f92b50b73d0ed97eebd272e94d9f7
Sebastian Pisarski <pisarski.sebastian@gmail.com>
2016-06-06 21:37:17 +0200

    add optional dropping of db schema

--- domain/src/main/java/com/gmail/sebastian/pisarski/configuration/DomainConfiguration.java
+++ domain/src/main/java/com/gmail/sebastian/pisarski/configuration/DomainConfiguration.java
@@ -1,10 +1,14 @@
 package com.gmail.sebastian.pisarski.configuration;
 
+import java.util.Properties;
+
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -14,10 +18,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
-@EnableJpaRepositories(basePackages = { "com.gmail.sebastian.pisarski.repository",
+@EnableJpaRepositories(basePackages = {
+		"com.gmail.sebastian.pisarski.repository",
 		"com.gmail.sebastian.pisarski.validator" })
 @EnableTransactionManagement
 public class DomainConfiguration {
+
+	@Autowired
+	private Environment env;
 
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
@@ -26,7 +34,18 @@ public class DomainConfiguration {
 		emfb.setDataSource(dataSource);
 		emfb.setJpaVendorAdapter(jpaVendorAdapter);
 		emfb.setPackagesToScan("com.gmail.sebastian.pisarski.entity");
+		setJpaProperties(emfb);
 		return emfb;
+	}
+
+	private void setJpaProperties(LocalContainerEntityManagerFactoryBean emfb) {
+		Boolean dropDbOnStar = env.getProperty("db.dropDbOnStar",
+				Boolean.class, false);
+
+		if (Boolean.TRUE.equals(dropDbOnStar)) {
+			Properties properties = new Properties();
+			properties.setProperty("hibernate.hbm2ddl.auto", "create");
+		}
 	}
 
 	@Bean
